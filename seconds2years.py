abf2213d99f24a681e61062249d9a378c0d72ccf
EddieWadors <wador101@mail.chapman.edu>
2016-02-18 13:57:55 -0800

    Create seconds2years.py

---
+++ seconds2years.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+File: seconds2years.py 
+
+Copyright (c) 2016 Eddie Wadors
+
+License: MIT
+
+Converting 10 billion seconds into years to determine whether a human could live that long.
+"""  
+years = float(10**9)/(365.25*24*60*60)
+print "%.3f years" % (years)
+print "the child will easily live to be"  "%.3f years" % (years)
